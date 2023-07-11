c35478351584cdf4e44282c9a5df7ab1043a1dda
Celil Ozturk <celil.ozturk@windowslive.com>
2023-07-11 23:44:37 +0200

    Register islemi Mail adresi Unique control

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -15,10 +15,18 @@ router.post("/register",async (req,res)=>{
         user._id=uuidv4();
         user.createdDate=new Date();
         user.isAdmin=false;
-        await user.save();
-        const token=jwt.sign({},secretKey,options);
-        let model={token:token,user:user};
-        res.json(model);
+
+        const checkUserEmail=await User.findOne({email:user.email});
+
+        if(checkUserEmail != null){
+            res.status(403).json({message:"Bu mail adresi daha once kullanilmis!"});
+        }
+        else{
+            await user.save();
+            const token=jwt.sign({},secretKey,options);
+            let model={token:token,user:user};
+            res.json(model);
+        }
     }
     catch(error){
         res.status(500).json({message:error.message});
