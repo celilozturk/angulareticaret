100018360435ce812bb56caa7942d8bf4d160695
Celil Ozturk <celil.ozturk@windowslive.com>
2023-07-11 23:50:56 +0200

    Login api metodu

--- backend/routers/auth.router.js
+++ backend/routers/auth.router.js
@@ -33,4 +33,28 @@ router.post("/register",async (req,res)=>{
     }
 })
 
+router.post("/login",async (req,res)=>{
+    try{
+        const{email,password}=req.body;
+
+        let user=await User.findOne({email:email});
+        if(user==null){
+            res.status(403).json({message:"Kullanici bulunamadi!"});
+
+        }
+        else{
+            if(user.password!=password){
+                res.status(403).json({message:"Sifre Yanlis!"});
+            }
+            else{
+                const token=jwt.sign({},secretKey,options);
+                let model={token:token,user:user}
+                res.json(model);
+            }
+        }
+    } catch(error){
+        res.status(500).json({message:error.message});
+    }
+})
+
 module.exports=router;
