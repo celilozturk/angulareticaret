50b7139e4380c72b8e54f917b84af27ec6f87bce
Celil Ozturk <celil.ozturk@windowslive.com>
2023-07-13 17:53:13 +0200

    Generic Blank Component

---
+++ frontend/src/app/common/components/blank/blank.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-blank',
+  standalone: true,
+  imports: [CommonModule,RouterModule],
+  templateUrl: './blank.component.html',
+  styleUrls: ['./blank.component.css']
+})
+export class BlankComponent {
+@Input() title:string="";
+@Input() sectionTitle:string="";
+
+}
