4bf1a555373a4f2c0c883bdd28e6155995a94326
Celil Ozturk <celil.ozturk@windowslive.com>
2023-07-13 13:48:09 +0200

    Register page

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { SharedModule } from 'src/app/common/shared/shared.module';
 
 @Component({
@@ -10,4 +11,7 @@ import { SharedModule } from 'src/app/common/shared/shared.module';
 })
 export class RegisterComponent {
 
+  register(form:NgForm){
+
+  }
 }
