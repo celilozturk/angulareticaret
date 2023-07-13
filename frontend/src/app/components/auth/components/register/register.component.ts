fa69f76fa16974d5b9b60c045825f2c8191d617c
Celil Ozturk <celil.ozturk@windowslive.com>
2023-07-13 14:08:17 +0200

    Register icin auth service

--- frontend/src/app/components/auth/components/register/register.component.ts
+++ frontend/src/app/components/auth/components/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { SharedModule } from 'src/app/common/shared/shared.module';
+import { RegisterModel } from '../../models/register.model';
+import { AuthService } from '../../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +15,18 @@ import { SharedModule } from 'src/app/common/shared/shared.module';
 })
 export class RegisterComponent {
 
-  register(form:NgForm){
+  model:RegisterModel=new RegisterModel();
+  constructor(private auth:AuthService,private toastr:ToastrService,private router:Router){
 
+  }
+  register(form:NgForm){
+    if(form.valid){
+      this.auth.register(this.model,res=>{
+        localStorage.setItem("token",res.token);
+        localStorage.setItem("user",JSON.stringify(res.user));
+        this.toastr.success("Kullanici kaydi basariyla tmamalandi")
+        this.router.navigateByUrl("/");
+      });
+    }
   }
 }
--- frontend/src/app/components/auth/services/auth.service.ts
+++ frontend/src/app/components/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { GenericHttpService } from 'src/app/common/shared/services/generic-http.service';
 import { LoginModel } from '../models/login.model';
 import { LoginResponseModel } from '../models/login-response.model';
+import { RegisterModel } from '../models/register.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,8 @@ export class AuthService {
 
   login(model:LoginModel,callBack:(res:LoginResponseModel)=>void){
     this.http.post<LoginResponseModel>("auth/login",model,res=>callBack(res));
+  }
+  register(model:RegisterModel,callBack:(res:LoginResponseModel)=>void){
+    this.http.post<LoginResponseModel>("auth/register",model,res=>callBack(res));
   }
 }
