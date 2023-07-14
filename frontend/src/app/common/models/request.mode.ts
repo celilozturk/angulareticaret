71ea2b2899d0c833c875481c59251e7113e0edbf
Celil Ozturk <celil.ozturk@windowslive.com>
2023-07-14 13:59:50 +0200

    Anasayfa HTML Tasariminin yapilmasi

--- frontend/src/app/common/models/request.mode.ts
+++ frontend/src/app/common/models/request.mode.ts
@@ -2,4 +2,6 @@ export class RequestModel{
     pageNumber:number=1;
     pagesize:number=10;
     search:string="";
+    categoryName:string="Tumu";
+    priceFilter:string="0";
 }
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/common/shared/shared.module';
+import { CategoryService } from '../categories/services/category.service';
+import { CategoryModel } from '../categories/models/category.model';
+import { RequestModel } from 'src/app/common/models/request.mode';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +12,20 @@ import { SharedModule } from 'src/app/common/shared/shared.module';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+categories:CategoryModel[]=[];
+request:RequestModel=new RequestModel();
 
+constructor(private _category:CategoryService){
+
+}
+  ngOnInit(): void {
+    this.getCategories();
+  }
+getCategories(){
+  this._category.getAll(res=>this.categories=res);
+}
+changeCategory(categoryName:string){
+  this.request.categoryName=categoryName;
+}
 }
